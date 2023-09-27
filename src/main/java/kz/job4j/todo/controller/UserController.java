4c42a27541732a4deea532dd5ce3577b8297ae8b
Aidos <aidos@paygate.kz>
2023-09-27 14:23:10 +0600

    add Registration and Authorization pages, controllers, services, repositories, model, db script, change headers

---
+++ src/main/java/kz/job4j/todo/controller/UserController.java
@@ -0,0 +1,62 @@
+package kz.job4j.todo.controller;
+
+import kz.job4j.todo.model.entity.User;
+import kz.job4j.todo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user, Model model, HttpServletRequest request) {
+        var userOptional =
+                userService.findByLoginAndPassword(user.getLogin(), user.getPassword());
+        if (userOptional.isEmpty()) {
+            model.addAttribute("error", "Почта или пароль введены неверно");
+            return "users/login";
+        }
+        var session = request.getSession();
+        session.setAttribute("user", userOptional.get());
+        model.addAttribute("user", userOptional.get());
+        return "redirect:/tasks";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String login(Model model, @ModelAttribute User user) {
+        var savedUser = userService.create(user);
+        if (savedUser == null) {
+            model.addAttribute("message", "Пользователь с такой почтой уже существует");
+            return "errors/404";
+        }
+        model.addAttribute("user", user);
+        return "users/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/users/login";
+    }
+}
